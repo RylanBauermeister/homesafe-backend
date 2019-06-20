3fd505332939bc167c7db2958147ed90b0936dec
Rylan Bauermeister <apple@MacBook-Pro-6.local>
2019-06-20 12:02:47 -0700

    safety check on coordinates

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,7 +3,7 @@ class ApplicationRecord < ActiveRecord::Base
 
   def self.coordsFromAddress(address)
     data = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{self.urlify(address)}&key=#{ENV["google_api_key"]}")
-    data["status"] != "ZERO_RESULTS" ? data["results"][0]["geometry"]["location"] : {}
+    data["results"] && data["status"] != "ZERO_RESULTS" ? data["results"][0]["geometry"]["location"] : {}
   end
 
   def self.urlify(address)
